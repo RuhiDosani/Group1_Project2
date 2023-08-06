82f12d7943a1e28d59005c49d3bf32f8122e9742
Kris Libre <kris.libre.kdp@gmail.com>
2023-08-06 17:04:59 -0400

    Edit UI, add date

--- app/src/main/java/com/example/group1_project2/BottomFragment.kt
+++ app/src/main/java/com/example/group1_project2/BottomFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class BottomFragment: Fragment(R.layout.bottom_segment) {
 
@@ -15,5 +18,8 @@ class BottomFragment: Fragment(R.layout.bottom_segment) {
 
         val animImg2 = AnimationUtils.loadAnimation(requireContext(), R.anim.rotate)
         wheelImageView.startAnimation(animImg2)
+
     }
+
+
 }
